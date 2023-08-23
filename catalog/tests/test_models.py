625e1ae7ae58f9f2b74ba75da6f7b622fdf8f907
sava9ecode <sava9emode@yandex.ru>
2023-08-23 16:30:00 +0300

    Add some more unit tests

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -98,7 +98,7 @@ class LanguageModelTest(TestCase):
         max_length = language._meta.get_field("name").max_length
         self.assertEqual(max_length, 200)
 
-    def test_genre_model_name_field_help_text(self):
+    def test_language_model_name_field_help_text(self):
         language = Language.objects.get(id=1)
         help_text = language._meta.get_field("name").help_text
         self.assertEqual(
@@ -115,7 +115,7 @@ class LanguageModelTest(TestCase):
 class BookModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        # Создание книги
+        # Create a book
         test_author = Author.objects.create(first_name="John", last_name="Smith")
         test_genre = Genre.objects.create(name="Fantasy")
         test_language = Language.objects.create(name="English")
@@ -128,15 +128,28 @@ class BookModelTest(TestCase):
         )
         # Create genre as a post-step
         genre_objects_for_book = Genre.objects.all()
-        test_book.genre.set(
-            genre_objects_for_book
-        )  # Присвоение типов many-to-many напрямую недопустимо
+        test_book.genre.set(genre_objects_for_book)
         test_book.save()
+
+    def test_title_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field("title").verbose_name
+        self.assertEqual(field_label, "title")
 
     def test_author_label(self):
         book = Book.objects.get(id=1)
         field_label = book._meta.get_field("author").verbose_name
         self.assertEqual(field_label, "author")
+
+    def test_summary_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field("summary").verbose_name
+        self.assertEqual(field_label, "summary")
+
+    def test_isbn_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field("isbn").verbose_name
+        self.assertEqual(field_label, "ISBN")
 
     def test_genre_label(self):
         book = Book.objects.get(id=1)
@@ -148,6 +161,111 @@ class BookModelTest(TestCase):
         field_label = book._meta.get_field("language").verbose_name
         self.assertEqual(field_label, "language")
 
+    def test_title_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field("title").max_length
+        self.assertEqual(max_length, 200)
+
+    def test_summury_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field("summary").max_length
+        self.assertEqual(max_length, 1000)
+
+    def test_isbn_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field("isbn").max_length
+        self.assertEqual(max_length, 13)
+
+    def test_summury_help_text(self):
+        book = Book.objects.get(id=1)
+        help_text = book._meta.get_field("summary").help_text
+        self.assertEqual(help_text, "Enter a brief description of the book")
+
+    def test_isbn_help_text(self):
+        book = Book.objects.get(id=1)
+        help_text = book._meta.get_field("isbn").help_text
+        self.assertEqual(
+            help_text,
+            '13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+            '">ISBN number</a>',
+        )
+
+    def test_genre_help_text(self):
+        book = Book.objects.get(id=1)
+        help_text = book._meta.get_field("genre").help_text
+        self.assertEqual(help_text, "Select a genre for this book")
+
+    def test_display_genre(self):
+        book = Book.objects.get(id=1)
+        self.assertEqual(book.display_genre(), "Fantasy")
+
+    def test_get_absolute_url(self):
+        book = Book.objects.get(id=1)
+        self.assertEqual(book.get_absolute_url(), "/catalog/book/1/")
+
+    def test_object_name_is_title(self):
+        book = Book.objects.get(id=1)
+        self.assertEqual(str(book), book.title)
+
 
 class BookInstanceModelTest(TestCase):
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        BookInstance.objects.create(imprint="test_imprint")
+
+    def test_id_label(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        field_label = bookinstance._meta.get_field("id").verbose_name
+        self.assertEqual(field_label, "id")
+
+    def test_book_label(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        field_label = bookinstance._meta.get_field("book").verbose_name
+        self.assertEqual(field_label, "book")
+
+    def test_imprint_label(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        field_label = bookinstance._meta.get_field("imprint").verbose_name
+        self.assertEqual(field_label, "imprint")
+
+    def test_due_back_label(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        field_label = bookinstance._meta.get_field("due_back").verbose_name
+        self.assertEqual(field_label, "due back")
+
+    def test_borrower_label(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        field_label = bookinstance._meta.get_field("borrower").verbose_name
+        self.assertEqual(field_label, "borrower")
+
+    def test_status_label(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        field_label = bookinstance._meta.get_field("status").verbose_name
+        self.assertEqual(field_label, "status")
+
+    def test_imprint_max_length(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        max_length = bookinstance._meta.get_field("imprint").max_length
+        self.assertEqual(max_length, 200)
+
+    def test_status_max_length(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        max_length = bookinstance._meta.get_field("status").max_length
+        self.assertEqual(max_length, 1)
+
+    def test_id_help_text(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        help_text = bookinstance._meta.get_field("id").help_text
+        self.assertEqual(
+            help_text, "Unique ID for this particular book across whole library"
+        )
+
+    def test_status_help_text(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        help_text = bookinstance._meta.get_field("status").help_text
+        self.assertEqual(help_text, "Book availability")
+
+    def test_status_default(self):
+        bookinstance = BookInstance.objects.get(imprint="test_imprint")
+        help_text = bookinstance._meta.get_field("status").default
+        self.assertEqual(help_text, "d")
--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -177,7 +177,7 @@ class LoanedBookInstancesByUserListViewTest(TestCase):
         self.assertEqual(len(response.context["bookinstance_list"]), 5)
 
     def test_pages_ordered_by_due_date(self):
-        # Change all books to be on loan
+        # Change all books to be on loan.
         for copy in BookInstance.objects.all():
             copy.status = "o"
             copy.save()
@@ -185,9 +185,9 @@ class LoanedBookInstancesByUserListViewTest(TestCase):
         login = self.client.login(username="testuser1", password="1X<ISRUkw+tuK")
         response = self.client.get(reverse("my-borrowed"))
 
-        # Check our user is logged in
+        # Check our user is logged in.
         self.assertEqual(str(response.context["user"]), "testuser1")
-        # Check that we got a response "success"
+        # Check that we got a response "success".
         self.assertEqual(response.status_code, 200)
 
         # Confirm that of the items, only 10 are displayed due to pagination.
@@ -203,7 +203,7 @@ class LoanedBookInstancesByUserListViewTest(TestCase):
 
 class RenewBookInstancesViewTest(TestCase):
     def setUp(self):
-        # Create a user
+        # Create a user.
         test_user1 = User.objects.create_user(
             username="testuser1", password="1X<ISRUkw+tuK"
         )
@@ -217,7 +217,7 @@ class RenewBookInstancesViewTest(TestCase):
         test_user2.user_permissions.add(permission)
         test_user2.save()
 
-        # Create a book
+        # Create a book.
         test_author = Author.objects.create(first_name="John", last_name="Smith")
         test_genre = Genre.objects.create(name="Fantasy")
         test_language = Language.objects.create(name="English")
@@ -228,12 +228,12 @@ class RenewBookInstancesViewTest(TestCase):
             author=test_author,
             language=test_language,
         )
-        # Create genre as a post-step
+        # Create genre as a post-step.
         genre_objects_for_book = Genre.objects.all()
         test_book.genre.set(genre_objects_for_book)
         test_book.save()
 
-        # Create a BookInstance object for test_user1
+        # Create a BookInstance object for test_user1.
         return_date = datetime.date.today() + datetime.timedelta(days=5)
         self.test_bookinstance1 = BookInstance.objects.create(
             book=test_book,
@@ -243,7 +243,7 @@ class RenewBookInstancesViewTest(TestCase):
             status="o",
         )
 
-        # Create a BookInstance object for test_user2
+        # Create a BookInstance object for test_user2.
         return_date = datetime.date.today() + datetime.timedelta(days=5)
         self.test_bookinstance2 = BookInstance.objects.create(
             book=test_book,
@@ -257,7 +257,7 @@ class RenewBookInstancesViewTest(TestCase):
         response = self.client.get(
             reverse("renew-book-librarian", kwargs={"pk": self.test_bookinstance1.pk})
         )
-        # Manually check redirect (Can't use assertRedirect, because the redirect URL is unpredictable)
+        # Manually check redirect (Can't use assertRedirect, because the redirect URL is unpredictable).
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response.url.startswith("/accounts/login/"))
 
@@ -283,7 +283,7 @@ class RenewBookInstancesViewTest(TestCase):
             reverse("renew-book-librarian", kwargs={"pk": self.test_bookinstance1.pk})
         )
 
-        # Check that it lets us login. We're a librarian, so we can view any users book
+        # Check that it lets us login. We're a librarian, so we can view any users book.
         self.assertEqual(response.status_code, 200)
 
     def test_uses_correct_template(self):
@@ -293,7 +293,7 @@ class RenewBookInstancesViewTest(TestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-        # Check we used correct template
+        # Check we used correct template.
         self.assertTemplateUsed(response, "catalog/book_renew_librarian.html")
 
     def test_form_renewal_date_initially_has_date_three_weeks_in_future(self):
@@ -349,7 +349,7 @@ class RenewBookInstancesViewTest(TestCase):
     def test_HTTP404_for_invalid_book_if_logged_in(self):
         import uuid
 
-        test_uid = uuid.uuid4()  # unlikely UID to match our bookinstance!
+        test_uid = uuid.uuid4()  # Unlikely UID to match our bookinstance!
         login = self.client.login(username="testuser2", password="2HJ1vRV0Z&3iD")
         response = self.client.get(
             reverse("renew-book-librarian", kwargs={"pk": test_uid})
@@ -361,7 +361,7 @@ class AuthorCreateViewTest(TestCase):
     """Test case for the AuthorCreate view (Created as Challenge)."""
 
     def setUp(self):
-        # Create a user
+        # Create a user.
         test_user1 = User.objects.create_user(
             username="testuser1", password="1X<ISRUkw+tuK"
         )
@@ -376,7 +376,7 @@ class AuthorCreateViewTest(TestCase):
         test_user2.user_permissions.add(permission)
         test_user2.save()
 
-        # Create an author
+        # Create an author.
         test_author = Author.objects.create(first_name="John", last_name="Smith")
 
     def test_redirect_if_not_logged_in(self):
@@ -415,6 +415,6 @@ class AuthorCreateViewTest(TestCase):
             reverse("author-create"),
             {"first_name": "Christian Name", "last_name": "Surname"},
         )
-        # Manually check redirect because we don't know what author was created
+        # Manually check redirect because we don't know what author was created.
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response.url.startswith("/catalog/author/"))
